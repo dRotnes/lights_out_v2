1bd238c00d28e03391d43b55d90da421a7e1c7c5
dRotnes <rotnesdante10@gmail.com>
2020-07-13 10:37:39 -0300

    IM BACK CARAIO

---
+++ lights_out_v2/Assets/Scripts/EnemyScripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyState
+{
+    idle,
+    chasing, 
+    attacking, 
+    returning,
+    dead
+}
+public class Enemy : MonoBehaviour
+{
+    public float health;
+    public float speed;
+    public float attackDamage;
+
+    public EnemyState currentState;
+    public Animator animator;
+    public FloatValue maxHealth;
+
+    private void Awake()
+    {
+        currentState = EnemyState.idle;
+        health = maxHealth.initialValue;
+    }
+    private void Update()
+    {
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+    public void TakeDamage(float damage)
+    {
+        animator.SetTrigger("Hit");
+        health -= damage;
+    }
+    public void Die()
+    {
+        currentState = EnemyState.dead;
+        animator.SetTrigger("Die");
+    }
+}
