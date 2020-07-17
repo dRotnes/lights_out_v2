75d35a1e885d1b5d593687a05360c6b68b21495d
dRotnes <rotnesdante10@gmail.com>
2020-07-17 18:34:22 -0300

    Ai Completed (fuck that bug)

--- lights_out_v2/Assets/Scripts/EnemyScripts/FollowerEnemy.cs
+++ lights_out_v2/Assets/Scripts/EnemyScripts/FollowerEnemy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using Pathfinding.RVO;
 public class FollowerEnemy : Enemy
 {
     public float startTimeBtwAttacks;
@@ -9,9 +10,7 @@ public class FollowerEnemy : Enemy
     public float attackRange;
     public LayerMask playerLayer;
     public Transform attackPoint;
-    private int counter;
 
-    private bool _canSeek = true;
     public Transform target;
     public GameObject player;
     public float speed;
@@ -21,7 +20,6 @@ public class FollowerEnemy : Enemy
     private Path _path;
     private int _currentWaypoint = 0;
     private bool _reachedEnd;
-    private bool _canChase;
     public GameObject startingPosition;
 
     private Seeker _seeker;
@@ -35,13 +33,11 @@ public class FollowerEnemy : Enemy
         target = startingPosition.transform;
         InvokeRepeating("UpdatePath", 0f, .5f);
         currentState = EnemyState.idle;
-        
-
     }
 
     void UpdatePath()
     {
-        if (_seeker.IsDone() && currentState != EnemyState.dead && _canSeek)
+        if (_seeker.IsDone() && currentState != EnemyState.dead)
         {
             _seeker.StartPath(_rb.position, target.position, OnPathComplete);
         }
@@ -99,8 +95,8 @@ public class FollowerEnemy : Enemy
         }
         Vector2 direction = ((Vector2)_path.vectorPath[_currentWaypoint] - _rb.position).normalized;
         Vector2 force = direction * speed * Time.deltaTime;
-
         _rb.AddForce(force);
+
 
         float playerDistance = Vector2.Distance(player.transform.position, transform.position);
         if(playerDistance <= chaseRadius && playerDistance > nextWaipointDistance)
@@ -155,156 +151,16 @@ public class FollowerEnemy : Enemy
             {
                 Debug.Log(collider.name);
                 GameObject player = collider.gameObject;
-                collider.gameObject.GetComponent<PlayerHealth>().TakeDamage(attackDamage / 8);
-                counter += 1;
+                player.GetComponent<PlayerHealth>().TakeDamage(attackDamage / 8);
             }
         }
     }
-}
-
-
-   /*
-
-    public float attackRange;
-    public LayerMask playerLayer;
-    public Transform attackPoint;
-
-    public float chasingDistance;
-    public float stoppingDistance;
-    public float attackingDistance;
-
-    private Rigidbody2D _rb;
-    private Transform _target;
-    private Vector3 _startingPosition;
-    private Vector2 _movement;
-
-
-    private bool _canAttack;
-    private bool _playerDead;
-    private SpriteRenderer _spriteRenderer;
-
-    private void Start()
-    {
-        currentState = EnemyState.idle;
-        _timeBtwAttacks = startTimeBtwAttacks;
-        _startingPosition = transform.position;
-        _target = GameObject.FindGameObjectWithTag("Player").transform;
-        _rb = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    private void Update()
-    {
-        if (currentState != EnemyState.chasing || currentState != EnemyState.returning)
-        {
-            _rb.Sleep();
-        }
-        else
-        {
-            _rb.WakeUp();
-        }
-        if (_target.GetComponent<PlayerMovement>().currentState == PlayerState.dead)
-        {
-            _playerDead = true;
-        }
-
-        if (health <= 0)
-        {
-            Die();
-        }
-
-        if (_canAttack && !_playerDead)
-        {
-            if (_timeBtwAttacks < -0)
-            {
-                animator.SetTrigger("Attack");
-                currentState = EnemyState.attacking;
-                _timeBtwAttacks = startTimeBtwAttacks;
-            }
-            else
-            {
-                _timeBtwAttacks -= Time.deltaTime;
-            }
-        }
-
-        if (_target.position.y > transform.position.y)
-        {
-            _spriteRenderer.sortingLayerName = "ForeGround";
-        }
-        else
-        {
-            _spriteRenderer.sortingLayerName = "Enemy";
-        }
-    }
-    private void FixedUpdate()
-    {
-        if (currentState != EnemyState.dead && currentState != EnemyState.attacking && !_playerDead)
-        {
-            CheckingDistance();
-        }
-        else
-        {
-            currentState = EnemyState.idle;
-        }
-    }
-
-    private void LateUpdate()
-    {
-        if (_movement != Vector2.zero)
-        {
-
-            animator.SetFloat("Horizontal", _movement.x);
-            animator.SetFloat("Vertical", _movement.y);
-        }
-        animator.SetFloat("Velocity", _movement.sqrMagnitude);
-
-    }
-
-    private void CheckingDistance()
-    {
-
-        float distance = Vector2.Distance(transform.position, _target.position);
-        if (distance <= chasingDistance && distance > stoppingDistance)
-        {
-            _movement = (_target.position - transform.position).normalized;
-            Vector3 temporaryMove = Vector2.MoveTowards(transform.position, _target.position, speed * Time.deltaTime);
-            _rb.MovePosition(temporaryMove);
-            _canAttack = false;
-            currentState = EnemyState.chasing;
-        }
-        else if (distance < stoppingDistance && distance > attackingDistance)
-        {
-            transform.position = this.transform.position;
-            _movement = Vector2.zero;
-            _canAttack = true;
-            currentState = EnemyState.idle;
-
-        }
-        else if (distance > chasingDistance)
-        {
-            _movement = (_startingPosition - transform.position).normalized;
-            Vector3 temporaryMove = Vector2.MoveTowards(transform.position, _startingPosition, speed * Time.deltaTime);
-            _rb.MovePosition(temporaryMove);
-            _canAttack = false;
-            currentState = EnemyState.returning;
-        }
-
-        if (transform.position == _startingPosition)
-        {
-            currentState = EnemyState.idle;
-        }
-    }
-
-    
-
 
     private void OnDrawGizmosSelected()
     {
-        if (attackPoint == null)
+        if (transform.position == null)
             return;
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-    }*/
-/*}*/
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
+    }
+}
 
-
-    
