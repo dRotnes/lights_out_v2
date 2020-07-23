b8c9260529a0217fbdd03644a9d9620f667c72e6
dRotnes <rotnesdante10@gmail.com>
2020-07-23 17:53:26 -0300

    Spike Trap Done

---
+++ lights_out_v2/Assets/Scripts/Traps Scripts/SpikeTrap.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : Trap
+{
+    private bool canActivate = true;
+    private bool isActive;
+    private float _damageRate = 0;
+
+    private void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Active"))
+        {
+            if (_damageRate <= 0)
+            {
+                Damage();
+                _damageRate = startDamageRate;
+            }
+            else
+            {
+                _damageRate -= Time.deltaTime;
+            }
+        }
+
+        switch (playerInRange) {
+            case true:
+                StopCoroutine("Desactivate");
+                switch (canActivate) {
+                    case true:
+                        isActive = true;
+                        canActivate = false;
+                        break;
+                    case false:
+                        return;
+                }
+                break;
+
+            case false:
+                _damageRate = 0;
+                switch (isActive)
+                {
+                    case true:
+                        StartCoroutine("Desactivate");
+                        break;
+                    case false:
+                        return;
+                }
+                break;
+        }
+
+        
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetBool("IsActive", isActive);
+    }
+    private void Damage()
+    {
+        if (playerCollider!=null)
+        {
+            playerCollider.GetComponentInParent<PlayerHealth>().TakeDamage(trapDamage);
+        }
+        else
+        {
+            Debug.Log("Yessir");
+        }
+    }
+
+    private IEnumerator Desactivate()
+    {
+        yield return new WaitForSeconds(timeActive);
+        isActive = false;
+        StartCoroutine("WaitToActivateAgain");
+    }
+    private IEnumerator WaitToActivateAgain()
+    {
+        yield return new WaitForSeconds(timeUnactive);
+        canActivate = true;
+
+    }
+}
---
+++ lights_out_v2/Assets/Scripts/Traps Scripts/Trap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    [Header("Trap Generals")]
+    public Animator animator;
+    public float timeActive;
+    public float timeUnactive;
+    public float trapDamage;
+    public float startDamageRate;
+    public bool playerInRange;
+    public Collider2D playerCollider;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("FootCollider"))
+        {
+            playerInRange = true;
+            playerCollider = collision;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("FootCollider"))
+        {
+            playerInRange = false;
+            playerCollider = null;
+        }
+    }
+}
